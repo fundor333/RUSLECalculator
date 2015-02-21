2d4c9439b26067c9bf778ca7902ec6a46669c1ae
Matteo Scarpa <matteoscarpa92@gmail.com>
2015-02-21 14:17:19 +0100

    Sistemato codice creazione raster 1/2

--- implementazione.py
+++ implementazione.py
@@ -1,12 +1,18 @@
 from random import randint
+import numpy
 
 __author__ = 'Fundor333'
 
 from qgis.core import QgsVectorLayer, QgsField, QgsFeature, QgsPoint, QgsGeometry, QgsVectorFileWriter, \
     QgsMapLayerRegistry
 from PyQt4.QtCore import QVariant
-from osgeo import gdal, ogr
-import numpy
+
+try:
+    from osgeo import *
+except:
+    import gdal
+    import ogr
+    import osr
 
 def add_element(pr, vl, i, j):
     fet = QgsFeature()
@@ -21,56 +27,47 @@ def run(dlg):
     if ncols == 0 | nrows == 0:
         print("La matrice non puo' essere costruita correttamente")
     else:
-        cellsize = 25
-        nodata_value = -9999
-        xllcorner = 0
-
-        source_layer = numpy.zeros((ncols, nrows))
+        matrix = numpy.zeros((ncols, nrows))
         for i in range(0, ncols):
             for j in range(0, nrows):
-                source_layer[i][j] = randint(0, 100)
-
-        # Create the destination data source
-        target_ds = gdal.GetDriverByName('GTiff').Create("test.tif", nrows, ncols, 1, gdal.GDT_Byte)
-        target_ds.SetGeoTransform((xllcorner, cellsize, 0, nrows * cellsize, 0, -cellsize))
-        band = target_ds.GetRasterBand(1)
-        band.SetNoDataValue(nodata_value)
-
-        # Rasterize
-        gdal.RasterizeLayer(target_ds, [1], source_layer, burn_values=[0])
+                matrix[i][j] = randint(0, 100)
+        create_raster("test.tif", 0.0, 0.0, ncols + 0.0, nrows + 0.0, matrix)
 
 
-def las(alt, larg):
-    vl = QgsVectorLayer("Point", "temporary_points", "memory")
-    pr = vl.dataProvider()
-
-    pr.addAttributes([QgsField("value", QVariant.Int)])
-
-    for i in range(0, larg):
-        for j in range(0, alt):
-            add_element(pr, vl, i, j)
-    vl.updateExtents()
-
-    # show some stats
-    print("fields: ", len(pr.fields()))
-    print("features: ", pr.featureCount())
-    e = vl.extent()
-    print(e.__class__.__name__)
-    print("extent:", "0", "0", alt, larg)
-    # iterate over features
-    renderer = vl.rendererV2()
-    for f in vl.getFeatures():
-        print("F:", f.id(), f.attributes(), f.geometry().asPoint())
-
-    # print(f.__class__.__name__)
-    # print("%f - %f: %s %s" % (
-    #                f.lowerValue(),
-    #                f.upperValue(),
-    #                f.label(),
-    #                str(f.symbol())
-    #            ))
-
-    # TODO Bisogna sistemare la visualizzazione grafica del plugin
-    QgsMapLayerRegistry.instance().addMapLayer(vl)
-
-
+def create_raster(filepath, orgX, orgY, pixWidth, pixHeight, array, proj=4326, gdal_type=gdal.GDT_Float32,
+                  nodata=-9999):
+    """
+    Creates an arbitrary raster
+    Args:
+    filepath (string): where to save file
+    orgX (float): x dimension origin coordinate (bottom left untransformed)
+    orgY (float): y dimension origin coordinate (bottom left untransformed)
+    pixWidth (float): size of each pixel's width (units depend on
+    projection)
+    pixHeight (float): size of each pixel's height (units depend on
+    projection)
+    array (np.array): raster values
+    Keyword Args:
+    proj (int): EPSG code
+    gdal_type (GDAL Datatype): a GDAL datatype
+    nodata (same as gdal_type): the NODATA value
+    Returns:
+    None
+    """
+    assert (len(array.shape) == 2)
+    num_bands = 1
+    rotX = 0
+    rotY = 0
+    rows = array.shape[0]
+    cols = array.shape[1]
+    driver = gdal.GetDriverByName('GTiff')
+    raster = driver.Create(filepath, cols, rows, num_bands, gdal_type)
+    print(driver.Create(filepath, cols, rows, num_bands, gdal_type))
+    raster.SetGeoTransform((orgX, pixWidth, rotX, orgY, rotY, pixHeight))
+    band = raster.GetRasterBand(1)  # Get only raster band
+    band.SetNoDataValue(nodata)
+    band.WriteArray(array)
+    raster_srs = osr.SpatialReference()
+    raster_srs.ImportFromEPSG(proj)
+    raster.SetProjection(raster_srs.ExportToWkt())
+    band.FlushCache()
