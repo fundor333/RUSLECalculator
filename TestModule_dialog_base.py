8483f061049119ae70855dfa21d921ff4b0914eb
Fundor333 <matteoscarpa92@gmail.com>
2015-02-26 10:51:46 +0100

    Aggiornato grafica

--- TestModule_dialog_base.py
+++ TestModule_dialog_base.py
@@ -2,8 +2,8 @@
 
 # Form implementation generated from reading ui file 'TestModule_dialog_base.ui'
 #
-# Created: Thu Feb 19 14:38:34 2015
-# by: PyQt4 UI code generator 4.11.2
+# Created: Thu Feb 26 10:49:05 2015
+#      by: PyQt4 UI code generator 4.10.4
 #
 # WARNING! All changes made in this file will be lost!
 
@@ -17,43 +17,41 @@ except AttributeError:
 
 try:
     _encoding = QtGui.QApplication.UnicodeUTF8
-
     def _translate(context, text, disambig):
         return QtGui.QApplication.translate(context, text, disambig, _encoding)
 except AttributeError:
     def _translate(context, text, disambig):
         return QtGui.QApplication.translate(context, text, disambig)
 
-
-class Ui_TestClassDialogBase(object):
-    def setupUi(self, TestClassDialogBase):
-        TestClassDialogBase.setObjectName(_fromUtf8("TestClassDialogBase"))
-        TestClassDialogBase.resize(337, 174)
-        self.button_box = QtGui.QDialogButtonBox(TestClassDialogBase)
+class Ui_SilvestriClassDialogBase(object):
+    def setupUi(self, SilvestriClassDialogBase):
+        SilvestriClassDialogBase.setObjectName(_fromUtf8("SilvestriClassDialogBase"))
+        SilvestriClassDialogBase.resize(337, 174)
+        self.button_box = QtGui.QDialogButtonBox(SilvestriClassDialogBase)
         self.button_box.setGeometry(QtCore.QRect(60, 110, 181, 32))
         self.button_box.setOrientation(QtCore.Qt.Horizontal)
-        self.button_box.setStandardButtons(QtGui.QDialogButtonBox.Cancel | QtGui.QDialogButtonBox.Ok)
+        self.button_box.setStandardButtons(QtGui.QDialogButtonBox.Cancel|QtGui.QDialogButtonBox.Ok)
         self.button_box.setObjectName(_fromUtf8("button_box"))
-        self.HighLabel = QtGui.QLabel(TestClassDialogBase)
+        self.HighLabel = QtGui.QLabel(SilvestriClassDialogBase)
         self.HighLabel.setGeometry(QtCore.QRect(50, 30, 111, 16))
         self.HighLabel.setObjectName(_fromUtf8("HighLabel"))
-        self.WidthLabel = QtGui.QLabel(TestClassDialogBase)
+        self.WidthLabel = QtGui.QLabel(SilvestriClassDialogBase)
         self.WidthLabel.setGeometry(QtCore.QRect(50, 70, 111, 16))
         self.WidthLabel.setObjectName(_fromUtf8("WidthLabel"))
-        self.highInput = QtGui.QSpinBox(TestClassDialogBase)
+        self.highInput = QtGui.QSpinBox(SilvestriClassDialogBase)
         self.highInput.setGeometry(QtCore.QRect(220, 20, 53, 25))
         self.highInput.setObjectName(_fromUtf8("highInput"))
-        self.widthInput = QtGui.QSpinBox(TestClassDialogBase)
+        self.widthInput = QtGui.QSpinBox(SilvestriClassDialogBase)
         self.widthInput.setGeometry(QtCore.QRect(220, 60, 53, 25))
         self.widthInput.setObjectName(_fromUtf8("widthInput"))
 
-        self.retranslateUi(TestClassDialogBase)
-        QtCore.QObject.connect(self.button_box, QtCore.SIGNAL(_fromUtf8("accepted()")), TestClassDialogBase.accept)
-        QtCore.QObject.connect(self.button_box, QtCore.SIGNAL(_fromUtf8("rejected()")), TestClassDialogBase.reject)
-        QtCore.QMetaObject.connectSlotsByName(TestClassDialogBase)
+        self.retranslateUi(SilvestriClassDialogBase)
+        QtCore.QObject.connect(self.button_box, QtCore.SIGNAL(_fromUtf8("accepted()")), SilvestriClassDialogBase.accept)
+        QtCore.QObject.connect(self.button_box, QtCore.SIGNAL(_fromUtf8("rejected()")), SilvestriClassDialogBase.reject)
+        QtCore.QMetaObject.connectSlotsByName(SilvestriClassDialogBase)
 
-    def retranslateUi(self, TestClassDialogBase):
-        TestClassDialogBase.setWindowTitle(_translate("TestClassDialogBase", "Test", None))
-        self.HighLabel.setText(_translate("TestClassDialogBase", "Altezza", None))
-        self.WidthLabel.setText(_translate("TestClassDialogBase", "Larghezza", None))
+    def retranslateUi(self, SilvestriClassDialogBase):
+        SilvestriClassDialogBase.setWindowTitle(_translate("SilvestriClassDialogBase", "Silvestri", None))
+        self.HighLabel.setText(_translate("SilvestriClassDialogBase", "Altezza", None))
+        self.WidthLabel.setText(_translate("SilvestriClassDialogBase", "Larghezza", None))
 
--- resources_rc.py
+++ resources_rc.py
@@ -2,8 +2,8 @@
 
 # Resource object code
 #
-# Created: lun feb 23 09:09:35 2015
-# by: The Resource Compiler for PyQt (Qt v4.8.6)
+# Created: gio feb 26 10:49:04 2015
+#      by: The Resource Compiler for PyQt (Qt v4.8.6)
 #
 # WARNING! All changes made in this file will be lost!
 
@@ -101,13 +101,10 @@ qt_resource_struct = "\
 \x00\x00\x00\x2c\x00\x00\x00\x00\x00\x01\x00\x00\x00\x00\
 "
 
-
 def qInitResources():
     QtCore.qRegisterResourceData(0x01, qt_resource_struct, qt_resource_name, qt_resource_data)
 
-
 def qCleanupResources():
     QtCore.qUnregisterResourceData(0x01, qt_resource_struct, qt_resource_name, qt_resource_data)
-
 
 qInitResources()
