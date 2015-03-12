2b0c8937906a29c8988a4b59cbc640b276c9fa62
Fundor333 <matteoscarpa92@gmail.com>
2015-03-12 10:56:35 +0100

    Sistemazione grafica

--- resources_rc.py
+++ resources_rc.py
@@ -2,7 +2,7 @@
 
 # Resource object code
 #
-# Created: gio mar 12 10:06:36 2015
+# Created: gio mar 12 10:56:09 2015
 #      by: The Resource Compiler for PyQt (Qt v4.8.6)
 #
 # WARNING! All changes made in this file will be lost!
