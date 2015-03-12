16609bd6720dc7280f69efacb8c278b5ef984706
Fundor333 <matteoscarpa92@gmail.com>
2015-03-12 10:09:31 +0100

    <3

--- resources_rc.py
+++ resources_rc.py
@@ -2,8 +2,8 @@
 
 # Resource object code
 #
-# Created: mer mar 11 10:40:58 2015
-# by: The Resource Compiler for PyQt (Qt v4.8.6)
+# Created: gio mar 12 10:06:36 2015
+#      by: The Resource Compiler for PyQt (Qt v4.8.6)
 #
 # WARNING! All changes made in this file will be lost!
 
@@ -101,13 +101,10 @@ qt_resource_struct = "\
 \x00\x00\x00\x2c\x00\x00\x00\x00\x00\x01\x00\x00\x00\x00\
 "
 
-
 def qInitResources():
     QtCore.qRegisterResourceData(0x01, qt_resource_struct, qt_resource_name, qt_resource_data)
 
-
 def qCleanupResources():
     QtCore.qUnregisterResourceData(0x01, qt_resource_struct, qt_resource_name, qt_resource_data)
-
 
 qInitResources()
