0360c5f72b2ffd7240701568a1ff3615725e91f4
Matteo Scarpa <matteoscarpa92@gmail.com>
2015-03-12 13:45:54 +0100

    Sistemazione esecutore

--- esecutore.py
+++ esecutore.py
@@ -3,4 +3,4 @@ __author__ = 'Fundor333'
 import os
 
 os.system('pyrcc4 -o resources_rc.py resources.qrc')
-os.system('pyuic4 -o TestModule_gui.py TestModule_gui.ui')
+os.system('pyuic4 -o MatrixModule_dialog_base.py MatrixModule_dialog_base.ui')
