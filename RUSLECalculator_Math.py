cfb0ff35d8e3da841ed77114ad8d9da82b75cf1b
fundor333 <fundor333@gmail.com>
2015-04-28 12:17:01 +0200

    Sistemazione math

--- RUSLECalculator_Math.py
+++ RUSLECalculator_Math.py
@@ -1,10 +1,11 @@
 from RUSLECalculator_lib import open_raster
 from osgeo.gdalnumeric import *
 from osgeo.gdalconst import *
+import math
 
 
-def rastermath(rl1, rl2, rl3, rl4, rl5, rl6, path_out, ras_type="GTiff"):
-    rl = rl1, rl2, rl3, rl4, rl5, rl6
+def rastermath(dem, fieldimage, k, r, p, c, path_out, ras_type="GTiff"):
+    rl = dem, fieldimage, k, r, p, c
     ds = range(0, 6)
     band = range(0, 6)
     data = range(0, 6)
@@ -15,11 +16,15 @@ def rastermath(rl1, rl2, rl3, rl4, rl5, rl6, path_out, ras_type="GTiff"):
         band[i] = ds[i].GetRasterBand(1)
         data[i] = BandReadAsArray(band[i])
 
-    dataOut = numpy.sqrt(data[0] + data[1] + data[2] + data[3] + data[4] + data[5])
+    dataOut = numpy.sqrt(data[2] * data[3] * data[4] * data[5])
 
     # Write the out file
     driver = gdal.GetDriverByName(ras_type)
-    dsOut = driver.Create(path_out, ds[0].RasterXSize, ds[0].RasterYSize, 1, band[0].DataType)
-    CopyDatasetInfo(ds[0], dsOut)
+    dsOut = driver.Create(path_out, k.RasterXSize, k.RasterYSize, 1, k.GetRasterBand(1).DataType)
+    CopyDatasetInfo(k, dsOut)
     bandOut = dsOut.GetRasterBand(1)
     BandWriteArray(bandOut, dataOut)
+
+
+def calcls(flowacc, cell_size, pend):
+    return ((flowacc * cell_size / 22.13) ^ 0.4) * (-1.5 + 17 / 1 + (math.e ^ (2.3 - 6.1 * math.sin(pend))))
