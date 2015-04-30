70386875317c34d1a087702ec94cdea5ad411f2b
Matteo Scarpa <matteoscarpa92@gmail.com>
2015-04-30 10:31:09 +0200

    Aggiunti gestione errori

--- RUSLECalculator_Exception.py
+++ RUSLECalculator_Exception.py
@@ -1,14 +1,36 @@
 __author__ = 'fundor333'
 
+
 class NoDem(Exception):
     def __init__(self, message):
         self.message = message
 
+
 class NoFieldImage(Exception):
     def __init__(self, message):
-        self.message=message
+        self.message = message
 
 
 class RasterError(Exception):
+    def __init__(self, message):
+        self.message = message
+
+
+class RError(Exception):
+    def __init__(self, message):
+        self.message = message
+
+
+class LSError(Exception):
+    def __init__(self, message):
+        self.message = message
+
+
+class CError(Exception):
+    def __init__(self, message):
+        self.message = message
+
+
+class KError(Exception):
     def __init__(self, message):
         self.message = message
